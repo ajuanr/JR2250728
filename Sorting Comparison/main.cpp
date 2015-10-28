ed5d7093d0888e9c210e9070bb35653ca4f547bb
Juan Ruiz <j.ruiz3750@gmail.com>
2015-10-28 16:18:23 -0700

    Signed off by Juan Ruiz

--- Sorting Comparison/main.cpp
+++ Sorting Comparison/main.cpp
@@ -8,67 +8,31 @@ void swap(int&, int&,int&,int&,int&,int&);
 void print(int [], const int&);
 void merge(int*,int,const int, const int,int&,int&,int&,int&);
 void merge_sort(int*, const int, const int,int&,int&,int&,int&);
-void heapsort(int [], int);
-void buildheap(int [], int);
-void heapify(int [], int, int);
+void heapsort(int [], int,int&,int&,int&,int&);
+void buildheap(int [], int,int&,int&,int&,int&);
+void heapify(int [], int, int,int&,int&,int&,int&);
 
 void filAray(int [],int);
 
 void quicksortAnalysis();
 void mergesortAnalysis();
-int main(){
-    //int test[] = { 7, -13, 1, 3, 10, 5, 2, 4 };
-    //int N = sizeof(test)/sizeof(int);
-    
-    int SIZE = 800;
-    int DBLSIZE=SIZE*2;
-    
-    int test[SIZE];
-    filAray(test, SIZE);
-    
-    int test2[DBLSIZE];
-    filAray(test2, DBLSIZE);
-    
+void heapsortAnalysis();
+
+int main(){    
     cout << "quicksort Analysis: " << endl;
     quicksortAnalysis();
     cout << endl;
     cout << "Merge sort analysis" << endl;
     mergesortAnalysis();
-//    cout << "Size of test array :"  << SIZE << endl;
+    cout << endl;
+    cout << "Heapsort analysis" << endl;
     
-//    cout << "Before sorting : " << endl;
-//    print(test, SIZE);
     int eq; // =
     int lop; // ==, <, <=, >, >=
     int lsa; // +, -, *, /
     int add; // []
     eq=lop=lsa=add=0;
-    //quickSort(test, 0, SIZE-1, eq,lop,lsa,add);
-    //merge_sort(test, 0, N-1);
-    //heapsort(test, N);
-    
-//    cout << endl << endl << "After sorting : " << endl;
-//    print(test, SIZE);
-    
-    //cout<<"Size of the array to sort = "<<SIZE<<endl;
-//    cout<<"Total number of equalities = "<<eq<<endl;
-//    cout<<"Total number of logical operations = "<<lop<<endl;
-//    cout<<"Total number of add/sub operations = "<<lsa<<endl;
-//    cout<<"Total number of addressing operations = "<<add<<endl;
-    //cout<<"Total number of operations = "<<eq+lop+lsa+add<<endl;
-    
-    
-    eq=lop=lsa=add=0;
-    quickSort(test2, 0, DBLSIZE-1, eq,lop,lsa,add);
-    //merge_sort(test, 0, N-1);
-    //heapsort(test, N);
-    
-//    cout<<"Size of the array to sort = "<<SIZE<<endl;
-//  cout<<"Total number of equalities = "<<eq<<endl;
-//  cout<<"Total number of logical operations = "<<lop<<endl;
-//  cout<<"Total number of add/sub operations = "<<lsa<<endl;
-//  cout<<"Total number of addressing operations = "<<add<<endl;
-//    cout<<"Total number of operations = "<<eq+lop+lsa+add<<endl;
+
     
     return 0;
 }
@@ -79,7 +43,8 @@ int main(){
  * @param first - The start of the sequence to be sorted.
  * @param last - The end of the sequence to be sorted.
  */
-void quickSort(int a[], int first, int last,int &eq, int &lop, int &lsa, int &add) {
+void quickSort(int a[], int first, int last,
+               int &eq, int &lop, int &lsa, int &add) {
     int pivotElement;
     ++lop;
     if(first < last)
@@ -106,7 +71,7 @@ int pivot(int a[], int first, int last,int &eq, int &lop, int &lsa, int &add) {
     ++eq; ++lsa;
     for(int i = first+1 ; i <= last ; i++)
     {
-        lop+=2; ++lsa;
+        lop+=2; ++lsa; ++add;
         /* If you want to sort the list in the other order, change "<=" to ">" */
         if(a[i] <= pivotElement)
         {
@@ -205,43 +170,61 @@ void merge_sort(int* A, const int p, const int r,int &eq, int &lop, int &lsa, in
     }
 }
 
-void heapsort(int A[], int length)//	       (takes O(n lg n) time)
+void heapsort(int A[], int length,int &eq, int &lop, int &lsa, int &add)//	       (takes O(n lg n) time)
 {
+    ++eq;
     int heapsize = length;
     
-    buildheap(A, length);	//Take the unsorted list and make it a heap
+    buildheap(A, length, eq,lop,lsa,add);	//Take the unsorted list and make it a heap
+    eq++; lsa++;
     for (int i = length-1; i >=1; i--)
     {
+        lop++; lsa+=2; add+=2;
         swap(A[0], A[i]);
         heapsize--;
-        heapify(A, heapsize, 0);
+        heapify(A, heapsize, 0, eq,lop,lsa,add);
     }
 }
 
-void buildheap(int A[], int length)	//     (takes O(n) time)
+void buildheap(int A[], int length,
+               int &eq, int &lop, int &lsa, int &add)	//     (takes O(n) time)
 {
-    for (int i = floor((length)/2); i >= 0 ; i--)
-        heapify(A, length, i);
+    eq++; lsa+=2;
+    for (int i = floor((length)/2); i >= 0 ; i--){
+        lop++; lsa++;
+        heapify(A, length, i, eq,lop,lsa,add);
+    }
 }
 
-void heapify(int A[], int heapsize, int root) //(takes O(h) h is the height of root
+void heapify(int A[], int heapsize, int root,
+             int &eq, int &lop, int &lsa, int &add) //(takes O(h) h is the height of root
 {
+    eq+=2; lsa+=4;
     int left = 2*root+1,
     right = 2*root +2,
     largest;
-    
-    if ( (left < heapsize) && (A[left] > A[root]))
+    lop+=2; add+=2;
+    if ( (left < heapsize) && (A[left] > A[root])) {
+        eq++;
         largest = left;
-    else
+    }
+    else {
+        eq++;
         largest = root;
+    }
     
-    if ( (right < heapsize) && (A[right] > A[largest]))
+    lop+=2; add+=2;
+    if ( (right < heapsize) && (A[right] > A[largest])) {
+        eq++;
         largest = right;
+    }
     
+    lop++;
     if (largest != root)
     {
+        add+=2;
         swap(A[root], A[largest]);
-        heapify(A, heapsize, largest);
+        heapify(A, heapsize, largest, eq,lop,lsa,add);
     }
 }
 
@@ -275,7 +258,7 @@ void quicksortAnalysis() {
     
     quickSort(test2, 0, DBLSIZE-1, eq,lop,lsa,add);
     int total2=eq+lop+lsa+add;
-    cout<<"Size of the array to sort = "<<SIZE<<endl;
+    cout<<"Size of the array to sort = "<<DBLSIZE<<endl;
     cout<<"Total number of operations = "<<total2 << endl;
     
     cout << "Ratio of operations:     " << total2/1.0/total1 << endl;
@@ -312,7 +295,44 @@ void mergesortAnalysis() {
     
     merge_sort(test2, 0, DBLSIZE-1, eq,lop,lsa,add);
     int total2=eq+lop+lsa+add;
+    cout<<"Size of the array to sort = "<<DBLSIZE<<endl;
+    cout<<"Total number of operations = "<<total2 << endl;
+    
+    cout << "Ratio of operations:     " << total2/1.0/total1 << endl;
+    cout << "Ratio of logs (nlog(n)): " << (DBLSIZE*log2(DBLSIZE))/(SIZE*log2(SIZE))
+    << endl;
+}
+
+void heapsortAnalysis() {
+    int SIZE = 800;
+    int DBLSIZE=SIZE*2;
+    
+    int test[SIZE];
+    filAray(test, SIZE);
+//  cout << "Before sorting : " << endl;
+//  print(test, SIZE);
+    
+    int eq; // =
+    int lop; // ==, <, <=, >, >=
+    int lsa; // +, -, *, /
+    int add; // []
+    eq=lop=lsa=add=0;
+    heapsort(test, SIZE, eq,lop,lsa,add);
+    
+//    cout << "After sorting : " << endl;
+//    print(test, SIZE);
+    
     cout<<"Size of the array to sort = "<<SIZE<<endl;
+    int total1=eq+lop+lsa+add;
+    cout<<"Total number of operations = "<<total1 <<endl;
+    
+    int test2[DBLSIZE];
+    filAray(test2, DBLSIZE);
+    eq=lop=lsa=add=0;
+    
+    heapsort(test, SIZE, eq,lop,lsa,add);
+    int total2=eq+lop+lsa+add;
+    cout<<"Size of the array to sort = "<<DBLSIZE<<endl;
     cout<<"Total number of operations = "<<total2 << endl;
     
     cout << "Ratio of operations:     " << total2/1.0/total1 << endl;
